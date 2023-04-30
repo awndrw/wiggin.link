b9ab77b9f67b83066c86f931ee78f0b57d1e4804
Andrew Wiggin <andrew@wiggin.dev>
2023-04-29 22:10:58 -0400

    Vercel permanent redirects instead of meta refresh

--- build.js
+++ build.js
@@ -1,8 +1,4 @@
 const fs = require('fs');
-const path = require('path');
-
-// Constants
-const OUT_DIR = './dist';
 
 const links = {
   wiggindev: 'https://github.com/wiggindev/-wiggindev/tree/main/packages',
@@ -14,19 +10,20 @@ const links = {
   twitter: 'https://twitter.com/wiggindev',
 };
 
-// Setup
-if (!fs.existsSync(OUT_DIR)) {
-  fs.mkdirSync(OUT_DIR);
-}
-
-// Build
-const template = fs.readFileSync('./template.html', 'utf8');
-
-const indexContent = template.replace('{{url}}', 'https://wiggin.dev');
-fs.writeFileSync(path.join(OUT_DIR, 'index.html'), indexContent);
+const redirects = [{
+  "source": "/",
+  "destination": "https://wiggin.dev",
+  "permanent": true
+}];
 
 Object.entries(links).forEach(([key, url]) => {
-  fs.mkdirSync(path.join(OUT_DIR, key));
-  const fileContent = template.replace('{{url}}', url);
-  fs.writeFileSync(path.join(OUT_DIR, key, 'index.html'), fileContent);
+  redirects.push({
+    "source": `/${key}`,
+    "destination": url,
+    "permanent": true
+  });
 });
+
+fs.writeFileSync("vercel.json", JSON.stringify({
+  redirects,
+}));
